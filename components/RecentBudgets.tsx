de66e50a49a553c24ebf712a87b9815e8bb4f25e
sadek1amine <148018682+sadek1amine@users.noreply.github.com>
2026-07-16 19:42:59 -0600

    Update RecentBudgets.tsx

--- components/RecentBudgets.tsx
+++ components/RecentBudgets.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import React, { useEffect, useState } from 'react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
